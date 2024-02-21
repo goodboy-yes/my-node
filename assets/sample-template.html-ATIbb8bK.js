6c191158de74a1cf0aeedc5ec06d3ab80736c1cf
goodboy-yes <goodboy-yes@users.noreply.github.com>
2024-02-21 08:27:01 +0000

    Update my-note cc2b5019ba09eb7184f82b0bd55c7c07288ae028

---
+++ assets/sample-template.html-ATIbb8bK.js
@@ -0,0 +1,36 @@
+import{_ as n,o as s,c as a,e as t}from"./app-CtA61ec0.js";const e={},p=t(`<h1 id="示例模板" tabindex="-1"><a class="header-anchor" href="#示例模板"><span>示例模板</span></a></h1><div class="language-html line-numbers-mode" data-ext="html" data-title="html"><pre class="language-html"><code><span class="token comment">&lt;!--html5文档申明--&gt;</span>
+<span class="token doctype"><span class="token punctuation">&lt;!</span><span class="token doctype-tag">DOCTYPE</span> <span class="token name">html</span><span class="token punctuation">&gt;</span></span>
+<span class="token comment">&lt;!--使用中文简体语言申明--&gt;</span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>html</span> <span class="token attr-name">lang</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>zh-CN<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>head</span><span class="token punctuation">&gt;</span></span>
+    <span class="token comment">&lt;!--字符编码--&gt;</span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>meta</span> <span class="token attr-name">charset</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>utf-8<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>
+    <span class="token comment">&lt;!--指明当前IE浏览器按最新的渲染引擎来渲染页面--&gt;</span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>meta</span> <span class="token attr-name">http-equiv</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>X-UA-Compatible<span class="token punctuation">&quot;</span></span> <span class="token attr-name">content</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>IE=edge<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>
+    <span class="token comment">&lt;!--视口：视口的宽度和设备一致 缩放比默认是1.0 不运行用户自行缩放--&gt;</span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>meta</span> <span class="token attr-name">name</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>viewport<span class="token punctuation">&quot;</span></span> <span class="token attr-name">content</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>width=device-width, initial-scale=1, user-scalable=0<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>
+    <span class="token comment">&lt;!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ --&gt;</span>
+    <span class="token comment">&lt;!-- 页面的解析顺序从上到小  优先告诉浏览器用最新的引擎来渲染并且这个一个移动端页面 --&gt;</span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>title</span><span class="token punctuation">&gt;</span></span>template<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>title</span><span class="token punctuation">&gt;</span></span>
+
+
+    <span class="token comment">&lt;!--html5标签和媒体查询在IE8一下浏览存在兼容问题，但是通过 html5shiv respond 俩解决--&gt;</span>
+    <span class="token comment">&lt;!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries --&gt;</span>
+    <span class="token comment">&lt;!--注意：不能以本地形式打开 可以http形式  webstorm 形式打开--&gt;</span>
+    <span class="token comment">&lt;!-- WARNING: Respond.js doesn&#39;t work if you view the page via file:// --&gt;</span>
+    <span class="token comment">&lt;!--html5shiv H5标签兼容--&gt;</span>
+    <span class="token comment">&lt;!--respond  媒体查询兼容--&gt;</span>
+
+    <span class="token comment">&lt;!--条件注释  在IE9以下生效--&gt;</span>
+    <span class="token comment">&lt;!--[if lt IE 9]&gt;
+    &lt;script src=&quot;../lib/html5shiv/html5shiv.min.js&quot;&gt;&lt;/script&gt;
+    &lt;script src=&quot;../lib/respond/respond.min.js&quot;&gt;&lt;/script&gt;
+    &lt;![endif]--&gt;</span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>head</span><span class="token punctuation">&gt;</span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>body</span><span class="token punctuation">&gt;</span></span>
+
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>h1</span><span class="token punctuation">&gt;</span></span>你好，世界！<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>h1</span><span class="token punctuation">&gt;</span></span>
+
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>body</span><span class="token punctuation">&gt;</span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>html</span><span class="token punctuation">&gt;</span></span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),l=[p];function c(o,i){return s(),a("div",null,l)}const m=n(e,[["render",c],["__file","sample-template.html.vue"]]),d=JSON.parse('{"path":"/html/sample-template.html","title":"示例模板","lang":"zh-CN","frontmatter":{},"headers":[],"git":{"updatedTime":1708503944000,"contributors":[{"name":"guokc","email":"guokc@tsintergy.com","commits":1}]},"filePathRelative":"html/sample-template.md"}');export{m as comp,d as data};
